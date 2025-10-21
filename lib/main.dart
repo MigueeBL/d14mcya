7ed79ab65c2b24bc3c1ef17636545f417452eee8
aitanaRyz <hernandezaitana321@hotmail.com>
2025-10-21 13:42:22 -0600

    Descripción de mis cambios

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text('Holaaaaaa dice tona que que haces?')),
-      ),
+      home: Scaffold(body: Center(child: Text('Holaaaaaawwfw wefwefw ?'))),
     );
   }
 }
