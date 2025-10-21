0d1502ba166dec74653f9e1d15332f40a6bb8bae
MigueeBL <ba498788@uaeh.edu.mx>
2025-10-21 13:16:15 -0600

    Cambie a Hola cara de bola

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hola mundo como estas!'),
+          child: Text('Hola cara de bola!'),
         ),
       ),
     );
