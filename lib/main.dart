1ec4504312be98be32d9f6d457fcb55cb02790af
MigueeBL <ba498788@uaeh.edu.mx>
2025-10-22 11:04:24 -0600

    puse jajajaj

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Text(
-            'Holaaaaaaaaaaaaa Andy no sabe hacer un tramite, se lo pedi hace mil años  ',
+            'Holaaaaaaaaaaaaa Andy no sabe hacer un tramite, se lo pedi hace mil años ajjajajajaj ',
           ),
         ),
       ),
